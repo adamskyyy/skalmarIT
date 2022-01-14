c4b9032de6f0dc31af527234fd7f95e4234bae4c
adamskyyy <adamskialeksander@gmail.com>
2022-01-14 12:57:51 +0100

    com1

---
+++ src/test/java/io/github/adamskyyy/zadanie/ZadanieApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.adamskyyy.zadanie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZadanieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
